8226e1e446c81292df6a3b5ce4ece47fec51a135
songokunr1 <pawelm18@gmail.com>
2018-02-28 16:29:18 +0100

    Create plansza.cpp

---
+++ .gitignore/plansza.cpp
@@ -0,0 +1,31 @@
+#include "Plansza.h"
+#include <iostream>
+
+Plansza::Plansza(int rozmiar_kw)
+{
+    rozmiar = rozmiar_kw
+}
+
+
+
+int Plansza::wypisz()
+{
+    a=5;
+    return a;
+}
+
+
+
+int Plansza::wybierz_pole_ruchu()
+{
+    return a;
+}
+
+int Plansza::sprawdz_czy_mina()
+{
+
+}
+
+int
+
+
